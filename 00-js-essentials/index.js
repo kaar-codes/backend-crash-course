f931210e8bd216b34d63cd0e7b337fe5ead8f3f1
kaar-codes <webdesignkaar@gmail.com>
2026-05-14 19:38:35 +0530

    Type Coercion

---
+++ 00-js-essentials/index.js
@@ -0,0 +1,31 @@
+console.log("Hello, World!");
+console.log("Welcome to JS Essentials");
+console.log("JS is Fun!...");
+
+// Data Types
+// number string boolean undefined null symbols bigint NaN
+// object arrays map set
+
+console.log(1000);
+console.log(1000 + 200);
+console.log((1000 + 300) * 2);
+
+console.log("LOGESH " + 1000);
+
+// Type COERCION - Implicit DATA TYPE DECLARATION CHANGE
+console.log("100" + 100);
+console.log("1000" - 100);
+console.log("LOGESH" - 100);
+console.log("100" + 3 * "2");
+
+console.log(String(100 < 200));
+
+console.log(Boolean(1230));
+console.log(Boolean(-123));
+console.log(Boolean(0));
+console.log(Boolean("LOGESH"));
+console.log(Boolean("202"));
+console.log(Boolean(""));
+
+console.log(1000 > 2000 < 1);
+console.log(true >= 1);
