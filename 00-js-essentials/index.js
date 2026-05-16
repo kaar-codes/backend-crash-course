4e5cd5589f37104c16d41852d863972bbf9301a4
kaar-codes <webdesignkaar@gmail.com>
2026-05-16 18:56:03 +0530

    Conditional Statements and Loops

--- 00-js-essentials/index.js
+++ 00-js-essentials/index.js
@@ -58,8 +58,120 @@ if (true) {
 }
 console.log(myName);
 
-*/
 
 myName = "LOGESH";
 
 console.log(myName);
+*/
+
+// CONDITONAL STATEMENTS
+// if (1000 > 10000) {
+//   console.log("If Block");
+// } else if (1000 > 10) {
+//   console.log("Else If Block");
+// }
+
+// FIZZ BUZZ
+// 3 - FIZZ
+// 5 - BUZZ
+// 3 and 5 FIZZ BUZZ
+
+// let num = prompt("Enter any number") * 1;
+
+// if (num) {
+//   if (num % 3 == 0 || num % 5 == 0) {
+//     if (num % 3 == 0 && num % 5 == 0) {
+//       console.log("FIZZ BUZZ");
+//     } else if (num % 5 == 0) {
+//       console.log("BUZZ");
+//     } else if (num % 3 == 0) {
+//       console.log("FIZZ");
+//     }
+//   } else {
+//     console.log(num + " is not divisible by either 3 or 5.");
+//   }
+// } else {
+//   console.log("It is not a Valid Input!");
+// }
+
+// let range = 30;
+// while (range) {
+//   let num = range;
+//   if (num % 3 == 0 && num % 5 == 0) {
+//     console.log("FIZZ BUZZ");
+//   } else if (num % 5 == 0) {
+//     console.log("BUZZ");
+//   } else if (num % 3 == 0) {
+//     console.log("FIZZ");
+//   } else {
+//     console.log(num);
+//   }
+//   range--;
+// }
+
+// console.log("----------- FOR OF LOOP ------------");
+// for (const letter of myText) {
+//   console.log(letter);
+// }
+
+// console.log("----------- FOR IN LOOP ------------");
+// for (let key in myText) {
+//   console.log(key);
+// }
+
+// let myText = "ABCDEFGHI";
+// console.log(myText.at(1));
+
+// let myNum = new Number(199);
+// console.dir(myNum);
+
+// for (const element of Object.keys([100, 120, 140, 200])) {
+//   console.log(element);
+// }
+
+// const myObj = {
+//   lastName: "TB",
+//   id: "001",
+//   firstName: "Logesh",
+// };
+
+// const myArr = ["001", "Logesh", "TB"];
+
+// for (const val of myArr) {
+//   console.log(val);
+// }
+
+// for (const val in myObj) {
+//   console.log(myObj[val]);
+// }
+
+// for (const element of "HELLO WORLD") {
+//   console.log("ABCDEFGHIJKLMNOPQRSTUVWXYZ ".indexOf(element));
+// }
+
+// TODO --
+
+// 1. Pick a Random Value
+// 2. Show - instead of each characters
+// 3. Ask the user to choose any letter
+// 4. Once he chooses check for the letter in the word
+// 5. If it is there and you have remaining lives replace that letter or Lose 1 lives till the game gets over.
+
+const qWord = "APPLE";
+let qWordHash = "";
+for (const key in qWord) {
+  qWordHash += "-";
+}
+console.log(qWordHash);
+let choice = prompt("Enter the Choice").toUpperCase();
+console.log(`You've have chosen ${choice}`);
+
+for (const [index, expectedLetter] of Object.entries(qWord)) {
+  if (expectedLetter == choice) {
+    console.log("You are right!");
+    const qWordSplitted = qWordHash.split();
+    qWordSplitted[index] = choice;
+    console.log(qWordSplitted);
+    qWordHash = qWordSplitted.join();
+  }
+}
