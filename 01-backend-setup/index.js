3150fb3d57ba5cbc80b5b55f1c3fd0d3f924da5c
kaar-codes <webdesignkaar@gmail.com>
2026-06-04 17:51:37 +0530

    Added Middlewares

--- 01-backend-setup/index.js
+++ 01-backend-setup/index.js
@@ -1,23 +1,14 @@
-import http from "node:http";
+import express from "express";
 
-http
-  .createServer((req, res) => {
-    if (req.url === "/about") {
-      res.writeHead(200, {
-        "Content-type": "text/html",
-      });
-      res.write("<h1>About Page</h1>");
-    } else if (req.url === "/products") {
-      res.writeHead(200, {
-        "Content-type": "text/html",
-      });
-      res.write("<h1>Products Page</h1>");
-    } else {
-      res.writeHead(404, { "Content-type": "text/html" });
-      res.write("404 Page Not Found");
-    }
-    res.end();
-  })
-  .listen(8000, "127.0.0.1", () =>
-    console.log("Started my server on http://127.0.0.1:8000"),
-  );
+const app = express();
+
+app.get("/", function (req, res) {
+  res.status(200).json({
+    message: "Successfully Reached End!",
+    customPoperty: req.customPoperty,
+  });
+});
+
+app.listen(8000, "127.0.0.1", () =>
+  console.log("Server is running on PORT 8000"),
+);
